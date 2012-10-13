f2616472140b255cc33656dd0656c645a19983fe
Dr Frink <sprucegum@gmail.com>
2012-10-12 19:14:02 -0700

    Refactored in-memory player list functions

--- frinkweb/serverstate.py
+++ frinkweb/serverstate.py
@@ -44,6 +44,84 @@ class ServerState(object):
 		
 		def pcount(self):
 			return len(self.opensessions)
-			
-		def pop_session(self,pname):
-			return self.opensessions[self.players[pname].name]
+
+
+        # Some Processing code.
+
+        def create_play_session(self,pname,ptime):
+            p = self.get_player(pname)
+            s = Session(player = p, start = ptime, end = ptime)
+            s.save()
+            self.opensessions[p.name] = s
+
+        def close_play_session(self,pname,ptime):
+            p = self.get_player(pname)
+            s = self.pop_session(p.name)
+            s.end = ptime
+            s.save()
+
+        def add_life(self,pname,kill,time):
+            if pname in self.openlives:
+                life = self.openlives[pname]
+                life.kill_set.add(kill)
+                life.save()
+            else:
+                p = self.players[pname]
+                life = Life(player = p, start = datetime.now())
+
+        def end_life(self,pname,time):
+            life = self.openlives[pname]
+            life.kill_set.add(kill)
+            life.save()
+
+        def get_player(self,pname):
+            pname = pname.decode('utf-8','ignore')
+            if pname in self.players:
+                return self.players[pname]
+            else:
+                for pkey in sorted(filter(lambda x: len(x)<len(pname),self.players.keys()),cmp=lambda x,y:cmp(len(y),len(x))):
+                    if pname.count(pkey):
+                        self.players[pname] = self.players[pkey]
+                        return self.players[pkey]
+                try:
+                    p = Player.objects.get(name=pname)
+                    self.players[pname] = p
+                    return p
+                except:
+                    try:
+                        p = Player.objects.get(printedname=pname)
+                        self.players[pname] = p
+                        if self.livelog:
+                            self.get_avatar(p)
+                        return p
+                    except:
+                        p = Player(name=pname,clan=self.get_clan('NoClan'))
+                        p.save()
+                        self.players[pname] = p
+                        if self.livelog:
+                            self.get_avatar(p)
+                        return p
+
+        def get_clan(self,cname):
+            if cname.decode('utf-8','ignore') in self.clans:
+                return self.clans[cname.decode('utf-8','ignore')]
+            else:
+                c, g = Clan.objects.get_or_create(name=cname.decode('utf-8','ignore'))
+                self.clans[cname.decode('utf-8','ignore')] = c
+                return c
+
+        def get_weapon(self,wname):
+            if wname in self.weapons:
+                return self.weapons[wname]
+            else:
+                w, g = Weapon.objects.get_or_create(name = wname)
+                self.weapons[wname] = w
+                return w
+
+        def get_cause(self,cname):
+            if cname in self.causes:
+                return self.causes[cname]
+            else:
+                c, created = Cause.objects.get_or_create(name = cname)
+                self.causes[cname] = c
+                return c
