e79e53df3c8eca7afef9987b8e0518b3041c1a7c
Dr Frink <frink@frinkarena.chewie>
2012-10-20 13:50:14 -0700

    Included a couple example styles

---
+++ frinkweb/stats/banner_settings.blue.py
@@ -0,0 +1,11 @@
+
+WATERMARK = "frink.gnudist.com"
+TRIM_COLOR = '#0070FF'
+BG_COLOR = '#002299'
+WATERMARK_COLOR = (0x0,0x70,0xFF)
+GOLD_COLOR = (0xFF,0xC6,0x00)
+GOLD_SHADOW = (0xA7,0x73,0x00)
+
+from sys import path
+path.append('../')
+from settings import KAG_DIR
